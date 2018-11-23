0e42760fe34e0c5601e512892b68c15c99e4505e
sergii <serge_11@ukr.net>
2018-11-23 23:34:40 +0200

    marged from work

---
+++ src/common.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+enum class HashType
+{
+    none,
+    md5, // $1$ is MD5
+    blowfish_a, // $2a$ is Blowfish
+    blowfish_y, // $2y$ is Blowfish
+    sha256, // $5$ is SHA-256
+    sha512, // $6$ is SHA-512
+};
+
+struct ShadowPassword
+{
+    HashType type;
+    std::string salt;
+    std::string hash;
+};
