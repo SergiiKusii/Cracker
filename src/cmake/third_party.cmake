baa9f5293a8d8bd37fad03afe25fcc7c53760652
Sergii Kusii <kusii.sergii@apriorit.com>
2018-12-05 14:17:15 +0200

    added boost to cmake

--- src/cmake/third_party.cmake
+++ src/cmake/third_party.cmake
@@ -1,8 +1,19 @@
+#crypto++
 find_package(CryptoPP REQUIRED)
 target_link_libraries(${PROJECT} cryptopp)
 
+#plog
 set(PLOG_INCLUDE_DIRS ${SOURCE_DIR}/plog/include)
 message(STATUS ${PLOG_INCLUDE_DIRS})
 target_include_directories(${PROJECT}
     PUBLIC ${PLOG_INCLUDE_DIRS}
     )
+
+#boost
+find_package(Boost REQUIRED COMPONENTS system) 
+if(Boost_FOUND)
+    include_directories(${Boost_INCLUDE_DIRS}) 
+    target_link_libraries(${PROJECT} ${Boost_LIBRARIES})
+else()
+    message(FATAL_ERROR "Boost is not found")
+endif()
