940e9998a037edcc4d0afae3abec3499f8449550
sergii <serge_11@ukr.net>
2018-12-08 22:27:50 +0200

    added test case

--- src/tests/CrackerTest.cpp
+++ src/tests/CrackerTest.cpp
@@ -6,14 +6,16 @@
 #include "Cracker.h"
 #include "EmptyRender.h"
 
-std::string g_password123 = "123";
+const std::string g_password1   = "1";
+const std::string g_password123 = "123";
+
+const PasswordHashInfo hashInfo1   = {HashType::md5, "12345678", "XM4P3PrKBgKNnTaqG9P0T/", false};
+const PasswordHashInfo hashInfo123 = {HashType::md5, "12345678", "tRy4cXc3kmcfRZVj4iFXr/", false};
 
 BOOST_AUTO_TEST_CASE( my_test )
 {
-   PasswordHashInfo hashInfo = {};
-
    Cracker cracker(std::make_unique<EmptyRender>());
-   auto password = cracker.Crack(hashInfo);
+   auto password = cracker.Crack(hashInfo1);
 
-   BOOST_CHECK( g_password123 == password );
+   BOOST_CHECK( g_password1 == password );
 }
