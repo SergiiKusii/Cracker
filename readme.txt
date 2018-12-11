1a52e5b565154a1d944badc713821d7c545e3e02
Sergii Kusii <kusii.sergii@apriorit.com>
2018-12-11 11:16:15 +0200

    fixed boost and ctypt linkage

--- readme.txt
+++ readme.txt
@@ -11,8 +11,9 @@ Extract it
 Open terminal and cd to extracted dir
 run "./bootstrap.sh --help" command to see help
 Then run "./bootstrap.sh" command it will take some time to build
-Then run "sudo ./b2 --with-test install" to install boost.
+Then run "sudo ./b2 --with-test --prefix=<path to install> install" to install boost.
 The default installation location are "/usr/local/inlcude/boost" for header files and "/usr/local/lib" for compiled libraries
+If we install boost in custom location we need to set BOOST_ROOT=<path to install>
 
 crypto++:
 apt-get install libcrypto++8 libcrypto++8-dbg libcrypto++-dev
--- src/cmake/third_party.cmake
+++ src/cmake/third_party.cmake
@@ -1,20 +1,17 @@
 #crypto++
-find_package(CryptoPP REQUIRED)
+# find_package(CryptoPP REQUIRED)
 
 #plog
 set(PLOG_INCLUDE_DIRS ${SOURCE_DIR}/plog/include)
 message(STATUS ${PLOG_INCLUDE_DIRS})
 include_directories(${PLOG_INCLUDE_DIRS})
 
-
 #boost 1.66 required
-# include(FindBoost)
-# set(Boost_USE_STATIC_LIBS ON)
-# find_package(Boost 1.66 REQUIRED COMPONENTS system filesystem thread program_options iostreams)
-# find_package(Boost REQUIRED) 
-# if(Boost_FOUND)
-#     include_directories(${Boost_INCLUDE_DIRS}) 
-#     target_link_libraries(${PROJECT} ${Boost_LIBRARIES})
-# else()
-#     message(FATAL_ERROR "Boost is not found")
-# endif()
+# set(BOOST_ROOT "/home/sergii/mnt/DEXT4/packages/boost_1_66_0/" CACHE PATH "Boost library path" )
+set(Boost_USE_STATIC_LIBS ON)
+find_package(Boost 1.66 REQUIRED COMPONENTS unit_test_framework)
+if(Boost_FOUND)
+    include_directories(${Boost_INCLUDE_DIRS}) 
+else()
+    message(FATAL_ERROR "Boost is not found")
+endif()
