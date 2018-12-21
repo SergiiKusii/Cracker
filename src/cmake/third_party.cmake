3a4fbd5e489af9ac37a56a4967f7e1bc0f226ce5
Sergii Kusii <kusii.sergii@apriorit.com>
2018-12-21 16:14:08 +0200

    refactor main function

--- src/cmake/third_party.cmake
+++ src/cmake/third_party.cmake
@@ -7,7 +7,7 @@ message(STATUS ${PLOG_INCLUDE_DIRS})
 include_directories(${PLOG_INCLUDE_DIRS})
 
 #boost 1.66 required
-# set(BOOST_ROOT "/home/sergii/mnt/DEXT4/packages/boost_1_66_0/" CACHE PATH "Boost library path" )
+set(BOOST_ROOT "/home/sergii/mnt/DEXT4/packages/boost_1_66_0/" CACHE PATH "Boost library path" )
 set(Boost_USE_STATIC_LIBS ON)
 find_package(Boost 1.66 REQUIRED COMPONENTS unit_test_framework program_options)
 if(Boost_FOUND)
